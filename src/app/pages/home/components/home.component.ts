7db92ba0c52dd8688941a73e5dcc448172715008
Alex Mwaura <alexmwaura36@gmail.com>
2024-10-14 11:06:31 +0300

    Updated HomeComponent HTML, SCSS, and TS files with new layout, styles, and data

--- src/app/pages/home/components/home.component.ts
+++ src/app/pages/home/components/home.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  menuItems = [
+    { name: 'Italian Sauce Mushroom', price: 19.00, image: 'assets/menu-item1.jpg' },
+    { name: 'Italian Sauce Mushroom', price: 19.00, image: 'assets/menu-item2.jpg' },
+    { name: 'Italian Sauce Mushroom', price: 19.00, image: 'assets/menu-item3.jpg' },
+    { name: 'Italian Sauce Mushroom', price: 19.00, image: 'assets/menu-item4.jpg' },
+  ];
 
+  galleryPhotos = [
+    'assets/gallery-photo1.jpg',
+    'assets/gallery-photo2.jpg',
+    'assets/gallery-photo3.jpg',
+    'assets/gallery-photo4.jpg',
+    'assets/gallery-photo5.jpg',
+    'assets/gallery-photo6.jpg',
+  ];
 }
